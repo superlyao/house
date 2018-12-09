1fae1f75ed5ceb28b058c7a41b7746d79bcc2ce8
yliao <superyao12@sina.com>
2018-12-09 22:09:26 +0800

    图片上传功能

--- src/main/java/com/yliao/house/web/controller/admin/AdminController.java
+++ src/main/java/com/yliao/house/web/controller/admin/AdminController.java
@@ -1,7 +1,16 @@
 package com.yliao.house.web.controller.admin;
 
+import com.yliao.house.base.ApiResponse;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
 
 /**
  * @Author: yliao
@@ -22,5 +31,27 @@ public class AdminController {
     @GetMapping("/admin/login")
     public String adminLoginPage() {
         return "admin/login";
+    }
+
+    @GetMapping("admin/add/house")
+    public String addHousePage() {
+        return "admin/house-add";
+    }
+
+    @PostMapping(value = "admin/upload/photo", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    @ResponseBody
+    public ApiResponse uploadPhoto(@RequestParam("file")MultipartFile file) {
+        if (file.isEmpty()) {
+            return ApiResponse.ofStatus(ApiResponse.Status.NOT_VALID_PARAM);
+        }
+        String fileName = file.getOriginalFilename();
+        File target = new File("E:/project/java/house/tmp/" + fileName);
+        try {
+            file.transferTo(target);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return ApiResponse.ofStatus(ApiResponse.Status.INTERNAL_SERVER_ERROR);
+        }
+        return ApiResponse.ofSuccess(null);
     }
 }
